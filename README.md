4287620fddd96906e75aa1c4aa1839944ea8157f
prince-ravi-leow <princeravileow@outlook.com>
2024-03-05 14:07:09 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,94 @@
+# ⏱️ `simpler_timer`: a simple interactive-first timer for all your Python timekeeping needs 
+# ❗️ TL;DR - or the impatient...
+```py
+from simpler_timer import SimplerTimer
+timer = SimplerTimer()
+timer.end()
+```
+Simple as that. Batteries included - no dependencies required. 🔋
+
+# ⚡️ Simple usage
+Say you want to time a simple operation. 
+
+First, import:
+```py
+from simpler_timer import SimplerTimer
+```
+Simply create a `SimplerTimer` object, run your operation(s), and call `.end()` to stop the timer and return elapsed time in seconds.
+
+From there, use the `.report()` method to print a nicely formatted timestamp.
+```py 
+>>> timer = SimplerTimer()
+>>> some_operation()
+>>> some_other_operation()
+>>> timer.end()
+0:00:15.005237
+>>> timer.report()
+Elapsed time (H:MM:SS.ff): 0:00:15.005237
+```
+
+Want to recall the non-formatted time in seconds? *Easy*:
+```py
+>>> elapsed_time = timer.recall()
+>>> elapsed_time
+15.005237
+```
+
+# 🍬 Other goodies
+## Monitor timer status / progress
+Use `.status` attribute or Boolean evaluation with `.is_running()` method:
+```py
+>>> timer = SimplerTimer()
+>>> timer.is_running()
+True
+>>> timer.end()
+>>> timer.status
+'Inactive'
+```
+Get progress readout of running timer:
+
+```py
+>>> timer.start()
+>>> timer.progress()
+2.3714890480041504
+>>> timer.progress()
+5.502876043319702
+>>> timer.progress()
+13.753906011581421
+```
+## Pause / resume
+Does what it says on the tin:
+```py
+>>> timer = SimpleTimer()
+>>> timer.pause()
+>>> timer.is_running()
+True
+>>> timer.resume()
+>>> timer.status
+'Active (resumed)'
+```
+> *Note that pausing/resuming affects `.status` attribute, but `is_active()` evaluates as `True`.*
+## Timestamps for humans
+Tired of reporting execution times with a bazillion decimal places?
+
+Worry no more - use `strip = True` to purge those pesky trailing digits with extreme prejudice.
+```py
+>>> timer.end()
+31.928856
+>>> timer.report(strip = True)
+'Elapsed time (H:MM:SS): 0:00:31'
+>>> timer.timestamp(strip = True)
+0:00:31
+```
+> *Currently only works for inactive timer with `report()` and `timestamp()` methods*.
+
+# ⛔️ What this tool is *not* (...or more specifically what it *is*)
+This is *not* a command line utility.
+
+I originally designed the `SimplerTimer` class for use in Python scripts / pipelines, where execution time could be calculated and reported in 2 short, ultra-readable statements. However, it tends to work great for interactive sessions, as having a single 'timer' object saves the headache of having to keep track of multiple intermediate variables. 
+
+While it's great for use within scripts and interactive sessions, this tool does not support being run FROM a CLI - and it most likely never will. Reason for this, is that the most simple and reliable version of this already exists in the the UNIX utility [`/usr/bin/time`](https://medium.com/hackernoon/usr-bin-time-not-the-command-you-think-you-know-34ac03e55cc3). If you want a proper **benchmarking** tool, Python's own [`timeit`](https://docs.python.org/3/library/timeit.html) will serve you well - and works both in interactive sessions, and as a command line utility. 
+
+Honourable mention to `PowerShell` [`Measure-Command`](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/measure-command?view=powershell-7.4) for our Windows friends.
+
+🐣 *Pssst - as an easter-egg, I've included the function that inspired this project: `simplerer_timer()`. This is an ultra-stripped down implementation, if all you want is a timer that starts, stops and reports elapsed time (inspect docstrings for usage)* 
---
+++ simpler_timer.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3
+
+import time
+from datetime import timedelta
+from math import modf
+
+class SimplerTimer:
+	"""
+
+	⏱️ SimplerTimer - a simple interactive-first timer for all your Python timekeeping needs
+
+	Simple usage:
+	>>> timer = SimplerTimer()
+	>>> my_awesome_function()
+	>>> timer.end()
+	>>> timer.report()
+	Elapsed time (H:MM:SS.ff): 0:00:03.005237
+	"""
+	def __init__(self):
+		"""
+		Initialise timer
+		"""
+		self._start_time = time.time()
+		self._elapsed_time = None
+		self.status = "Active"
+		self._durations = []
+
+	def __str__(self):
+		return f'Timer status: {self.status}'
+	
+	def is_active(self):
+		"""
+		Boolean evaluation of timer activity status
+		"""
+		if self._start_time:
+			return True
+		else:
+			return False	
+
+	def start(self):
+		"""
+		(Re)-start timer (requires timer to be ended beforehand)
+		"""
+		if not self._elapsed_time:
+			raise Exception("Timer not ended. Call .end() to stop.")
+		self._start_time = time.time()
+		self._elapsed_time = None
+		self.status = "Active"
+
+	def pause(self):
+		"""
+		Pause running timer (changes status to: Paused)
+		"""
+		if not self.is_active():
+			raise Exception("Timer inactive. Call .start() to start.")
+		self._durations.append(time.time())
+		self.status = "Paused"
+
+	def resume(self):
+		"""
+		Resume timer (changes status to: Active (resumed))
+		"""
+		if not self.is_active():
+			raise Exception("Timer inactive. Call .start() to start.")
+		self._durations.append(time.time())
+		self.status = "Active (resumed)"
+
+	def end(self):
+		"""
+		Stop the timer and return elapsed time (SS.ff)
+		"""
+		if not self._start_time:
+			raise Exception("Timer not running. Call .start() to initiate.")
+		if self.status == "Paused":
+			self._durations.append(time.time())
+		if len(self._durations) == 0:
+			self._elapsed_time = time.time() - self._start_time
+			self._start_time = None
+			self.status = "Inactive"
+		else:
+			end = time.time()
+			diff = 0
+			odd_ts = self._durations[::2]
+			even_ts = self._durations[1::2]
+			for odd,even in zip(odd_ts,even_ts):
+				diff += even - odd
+			self._elapsed_time = end - diff - self._start_time
+			self._start_time = None
+			self.status = "Inactive"
+			self._durations = []
+			
+		return self._elapsed_time
+	
+	def progress(self):
+		"""
+		Report progress of running timer
+		"""
+		if not self._start_time:
+			raise Exception("Timer not running. Call .start() to initiate.")
+		if len(self._durations) == 0:
+			prog = time.time() - self._start_time
+		else:
+			end = time.time()
+			diff = 0
+			odd_ts = self._durations[::2]
+			even_ts = self._durations[1::2]
+			for odd,even in zip(odd_ts,even_ts):
+				diff += even - odd
+			prog = end - diff - self._start_time
+			
+		return prog	
+	
+	def recall(self):
+		"""
+		Recall elapsed time (SS.ff)
+		"""
+		if not self._elapsed_time:
+			raise Exception("Timer not ended. Call .end() to stop.")
+		return self._elapsed_time
+
+	def report(self, strip:bool = None):
+		"""
+		Report execution time (H:MM:SS.ff)
+		"""
+		if not self._elapsed_time:
+			raise Exception("Timer not ended. Call .end() to stop.")
+		if not strip:
+			seconds = self._elapsed_time
+			msg = "Elapsed time (H:MM:SS.ff):"
+		else:
+			seconds = int(modf(self._elapsed_time)[1])
+			msg = "Elapsed time (H:MM:SS):"
+		exec_time = str(timedelta(seconds = seconds))
+		print(f"{msg} {exec_time}")
+
+	def timestamp(self, strip:bool = None):
+		"""
+		Convert stored elapsed time float to H:MM:SS.ff-formatted timestamp
+		"""
+		if not self._elapsed_time:
+			raise Exception("Timer not ended. Call .end() to stop.")
+		if not strip:
+			seconds = self._elapsed_time
+		else:
+			seconds = int(modf(self._elapsed_time)[1])
+		return str(timedelta(seconds = seconds))	
+	
+def simplerer_timer(start:float = None) -> float:
+	"""
+	*** (even) simpler timer ***
+	Usage:
+	>>> from simpler_timer import simpler_timer
+	>>> start = simpler_timer()
+	>>> my_awesome_function()
+	>>> simpler_timer(start)
+	Elapsed time (H:MM:SS.ff): 0:00:01.340824
+	1.3408238887786865
+	"""
+
+	if not start:
+		return time.time()
+	else:
+		elapsed_time = time.time() - start
+		elapsed_time_string = str(timedelta(seconds = elapsed_time))
+		print(f"Elapsed time (H:MM:SS.ff): {elapsed_time_string}")
+		return elapsed_time
